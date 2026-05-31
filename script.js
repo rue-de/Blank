081a20225af1736670c2ce94b77a67b041fabaa7
rue-de <bradysealey8@gmail.com>
2026-05-31 16:31:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.getElementById("stats").innerHTML = `
+HP: 100/100<br>
+Attack: 10<br>
+Floor: 1
+`;
 
+document.getElementById("game").innerHTML = `
+<h2>Tower Reborn Prototype</h2>
+
+<p>Your first GitHub-hosted version is working.</p>
+
+<button onclick="alert('The Tower Awaits')">
+Enter Tower
+</button>
+`;
